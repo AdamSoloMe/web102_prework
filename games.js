788ce7a6bcdfa61f6aaeda4461921d1c81429615
adam_solomon_home <adamsolomon1801@gmail.com>
2026-03-31 22:43:15 -0400

    updates

--- games.js
+++ games.js
@@ -1,5 +1,6 @@
 // Image credits: Kickstarter
 const games = ` [
+
   {
     "name": "Heroes Of Mythic Americas",
     "description": "An exciting 5e RPG supplement that heroically represents pre-Columbian American cultures and mythologies",
